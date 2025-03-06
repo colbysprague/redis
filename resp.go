62776a244dbaa9c61aacabd02dac08a1218ed3fa
colby.sprague <colbyjsprague@gmail.com>
2025-03-05 21:50:42 -0500

    fix build err and use RESP in server

--- resp.go
+++ resp.go
@@ -8,62 +8,49 @@ import (
 )
 
 const (
-    STRING  = '+'
-    ERROR   = '-'
-    INTEGER = ':'
-    BULK    = '$'
-    ARRAY   = '*'
-
-    TYPE_BULK = "bulk"
-    TYPE_ARRAY = "array"
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
 )
 
 type Value struct {
-    typ   string  // data type of the value
-    str   string  // holds value of string from simple strings
-    num   int     // holds value of int from integers
-    bulk  string  // holds value of strings from bulk strings
-    array []Value // holds all valyes recieved from arrays
+	typ   string
+	str   string
+	num   int
+	bulk  string
+	array []Value
 }
 
-
 type Resp struct {
-    reader *bufio.Reader
+	reader *bufio.Reader
 }
 
 func NewResp(rd io.Reader) *Resp {
-    return &Resp{reader: bufio.NewReader(rd)}
+	return &Resp{reader: bufio.NewReader(rd)}
 }
 
-// reads one byte at a time until we reach '\r' indicating the end of a line 
-// then, we return the line w/o the last two bytes \r\n, and n number of bytes in the line
 func (r *Resp) readLine() (line []byte, n int, err error) {
-    for {
-        // read the byte
-        b, err := r.reader.ReadByte()
-        if err != nil {
-            return nil, 0, err
-        }
-
-        // inc count and add byte to []byte
-        n += 1
-        line = append(line, b)
-        if len(line) >= 2 && line[len(line)-2] == '\r' {
-            break
-        }
-    }
-    return line[:len(line)-2], n, nil
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+	return line[:len(line)-2], n, nil
 }
 
-// parse single integer from the reader
-// x (integer) n (num bytes read) err (error)
 func (r *Resp) readInteger() (x int, n int, err error) {
-
 	line, n, err := r.readLine()
 	if err != nil {
 		return 0, 0, err
 	}
-
 	i64, err := strconv.ParseInt(string(line), 10, 64)
 	if err != nil {
 		return 0, n, err
@@ -89,70 +76,49 @@ func (r *Resp) Read() (Value, error) {
 	}
 }
 
-// Skip the first byte because we have already read it in the Read method.
-// Read the integer that represents the number of elements in the array.
-// Iterate over the array and for each line, call the Read method to parse the type according to the character at the beginning of the line.
-// With each iteration, append the parsed value to the array in the Value object and return it
-
-// method to recursively read array
 func (r *Resp) readArray() (Value, error) {
+	v := Value{}
+	v.typ = "array"
 
-    v := Value{}
-    v.typ = TYPE_ARRAY
-    
-    // skip the first byte
-    r.reader.ReadByte()
+	// read length of array
+	len, _, err := r.readInteger()
+	if err != nil {
+		return v, err
+	}
 
-    // Read the integer representing n of elements in the array
-    length, _, err := r.readInteger()
-    if err != nil {
-        panic("readArray: Could not read n from []byte")
-    }
+	// foreach line, parse and read the value
+	v.array = make([]Value, 0)
+	for i := 0; i < len; i++ {
+		val, err := r.Read()
+		if err != nil {
+			return v, err
+		}
 
+		// append parsed value to array
+		v.array = append(v.array, val)
+	}
 
-    // iterate over the array and call read on each element and append it to the val
-
-    for _ = range length {
-        readVal, err := r.Read()
-        if err != nil {
-            return v, err
-        }
-
-        v.array = append(v.array, readVal) 
-    }
-
-   return v, nil 
+	return v, nil
 }
 
-// Skip the first byte because we have already read it in the Read method.
-// Read the integer that represents the number of bytes in the bulk string.
-// Read the bulk string, followed by the ‘\r\n’ that indicates the end of the bulk string.
-// Return the Value object.
+func (r *Resp) readBulk() (Value, error) {
+	v := Value{}
 
-// method to recursively read Bulk ?
-func (r *Resp) readBulk() (Value, error){
-    v := Value{}
-    v.typ = TYPE_BULK
-    // skip the first byte since we've already read it
-    r.reader.ReadByte()
+	v.typ = "bulk"
 
-    
-    // read the integer that represents the number of bytes in the bulk string
-    length, _, err := r.readInteger()
-    if err != nil {
-        return v, err
-    }
-    
-    // allocate []byte for bulk
-    bulk := make([]byte, length)
-    
-    // read from reader into bulk "records data into p"
-    r.reader.Read(bulk)
+	len, _, err := r.readInteger()
+	if err != nil {
+		return v, err
+	}
 
-    v.bulk = string(bulk)
+	bulk := make([]byte, len)
 
-    // read off \r\n
-    r.readLine()
+	r.reader.Read(bulk)
 
-    return v, nil
+	v.bulk = string(bulk)
+
+	// Read the trailing CRLF
+	r.readLine()
+
+	return v, nil
 }
